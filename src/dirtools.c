c10a38b23a297d93593f7be46680c4fd80b75d3b
Paper <37962225+mrpapersonic@users.noreply.github.com>
2022-08-07 22:47:41 -0400

    dirtools: sys/stat.h->windows.h

--- src/dirtools.c
+++ src/dirtools.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #ifndef WIN32_LEAN_AND_MEAN
 #  define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
 #include "dirtools.h"
 
+int dirtools_directory_exists(char* path) {
+	DWORD attrib = GetFileAttributesA(path);
+	return (attrib != INVALID_FILE_ATTRIBUTES && (attrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
 int dirtools_create_directory(char* path) {
-	struct stat st = {0};
 	char* alltoks = calloc(strlen(path), sizeof(char)), *tok;
 
 	for (tok = strtok(path, "\\"); tok != NULL; tok = strtok(NULL, "\\")) {
 		strcat(alltoks, tok);
-		if (stat(alltoks, &st) == -1) {
+		if (dirtools_directory_exists(path)) {
 			if (!CreateDirectoryA(alltoks, NULL)) {
 				if (GetLastError() == ERROR_PATH_NOT_FOUND) {
 					/* ERROR_PATH_NOT_FOUND should NOT happen here */
